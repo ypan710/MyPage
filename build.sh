eae46ff56060db46621ef3d810158ea7af04339b
Yongjian Pan <yongjianpan@Yongjians-MacBook-Air.local>
2019-06-20 19:59:27 -0700

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html content/bio.html templates/bottom.html > docs/bio.html
+cat templates/top.html content/project.html templates/bottom.html > docs/project.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/content.html
