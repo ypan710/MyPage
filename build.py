5991c056993e75b77deff897803559317468f54d
Yongjian Pan <yongjianpan@Yongjians-MacBook-Air.local>
2019-06-27 18:57:47 -0700

    Refactored my Python template script

--- build.py
+++ build.py
@@ -1,17 +1,69 @@
-head1 = open("templates/top.html").read()
-body1 = open("content/bio.html").read()
-bottom1 = open("templates/bottom.html").read()
-full1 = head1 + body1 + bottom1
-open("docs/bio1.html", "w+").write(full1)
+pages = [
+        {
+            "filename": "content/bio.html",
+            "output": "docs/bio.html",
+            "title": "About Me",
+        },
+        {
+            "filename": "content/project.html",
+            "output": "docs/project.html",
+            "title": "My Projects",
+        },
+        {
+            "filename": "content/contact.html",
+            "output": "docs/contact.html",
+            "title": "Contact me",
+        },
+    ]
 
-head2 = open("templates/top.html").read()
-body2 = open("content/project.html").read()
-bottom2 = open("templates/bottom.html").read()
-full2 = head2 + body2 + bottom2
-open("docs/project1.html", "w+").write(full2)
+def apply_template(template,index_content):
+    result = template.replace("{{Content}}", index_content)
+    return result
 
-head3 = open("templates/top.html").read()
-body3 = open("content/contact.html").read()
-bottom3 = open("templates/bottom.html").read()
-full3 = head3 + body3 + bottom3
-open("docs/contact1.html", "w+").write(full3)
+def read_template():
+    template = open("templates/base.html").read()
+    return template
+
+def main():
+
+    # loop through a list
+    for page in pages:
+
+        # access data within dictionary
+        page_name = page["filename"]
+        page_output = page["output"]
+        page_title = page["title"]
+
+#        # open and read the files
+#        top = open("templates/top.html").read()
+#        content = open(page_name).read()
+#        bottom = open("templates/bottom.html").read()
+#        full = top + content + bottom
+#        open(page_output, "w+").write(full)
+
+        # read in the entire template
+        template = read_template()
+        
+        # read in the content of the index HTML page
+        index_content = open(page_name).read()
+        
+        # use the string replace
+        finished_index_page = apply_template(template, index_content)
+        open(page_output, "w+").write(finished_index_page)
+
+        
+
+    #top = open("templates/top.html").read()
+    #project = open("content/project.html").read()
+    #bottom = open("templates/bottom.html").read()
+    #full_project = top + project + bottom
+    #open("docs/project1.html", "w+").write(full_project)
+
+    #top = open("templates/top.html").read()
+    #contact = open("content/contact.html").read()
+    #bottom = open("templates/bottom.html").read()
+    #full_contact = top + contact + bottom
+    #open("docs/contact1.html", "w+").write(full_contact)
+
+if __name__ == "__main__":
+    main()
